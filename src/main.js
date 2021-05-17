c65adfc651c0a1693f5784491cb8a1c2d6d31948
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2021-05-17 20:33:56 +0100

    install vue-tour

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store/index'
 import vuetify from './plugins/vuetify';
+import VueTour from 'vue-tour';
+import 'vue-tour/dist/vue-tour.css';
 
+Vue.use(VueTour);
 Vue.config.productionTip = false
 
 new Vue({
